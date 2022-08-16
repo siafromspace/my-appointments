d3696fded25807929c43867424151b20eaff6e9e
siafromspace <sharonyak15@gmail.com>
2022-08-16 15:58:39 +0100

    local storage

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,24 +2,25 @@ import { useContext, useState } from "react";
 import { BiSearch, BiArrowToBottom, BiCheck } from "react-icons/bi"
 import { AptListContext } from "../contexts/AptListContext";
 
-const DropDown = ({ toggle }) => {
+const DropDown = ({ toggle, sortBy, orderBy, onChangeSortBy, onChangeOrderBy }) => {
 
     if (!toggle) {
         return null
     }
     return (
         <ul className="absolute bg-white top-12 right-0 shadow-md rounded-md w-60 z-10">
-            <li className="cursor-pointer px-4 text-neutral-500 hover:bg-neutral-400 hover:text-white py-2">Title <BiCheck className="inline float-right text-neutral-900" /></li>
-            <li className="cursor-pointer px-4 text-neutral-500 hover:bg-neutral-400 hover:text-white py-2">Date <BiCheck className="inline float-right text-neutral-900" /></li>
+            <li onClick={() => onChangeSortBy("aptTitle")} className="cursor-pointer px-4 text-neutral-500 hover:bg-neutral-400 hover:text-white py-2">Title{sortBy === "aptTitle" && <BiCheck className="inline float-right text-neutral-900" />}</li>
+            <li onClick={() => onChangeSortBy("aptDate")} className="cursor-pointer px-4 text-neutral-500 hover:bg-neutral-400 hover:text-white py-2">Date {sortBy === "aptDate" && <BiCheck className="inline float-right text-neutral-900" />}</li>
+            <li onClick={() => onChangeSortBy("aptLocation")} className="cursor-pointer px-4 text-neutral-500 hover:bg-neutral-400 hover:text-white py-2">Location {sortBy === "aptLocation" && <BiCheck className="inline float-right text-neutral-900" />}</li>
             <hr />
-            <li className="cursor-pointer px-4 text-neutral-500 hover:bg-neutral-400 hover:text-white py-2">Asc <BiCheck className="inline float-right text-neutral-900" /></li>
-            <li className="cursor-pointer px-4 text-neutral-500 hover:bg-neutral-400 hover:text-white py-2">Desc <BiCheck className="inline float-right text-neutral-900" /></li>
+            <li onClick={() => onChangeOrderBy("asc")} className="cursor-pointer px-4 text-neutral-500 hover:bg-neutral-400 hover:text-white py-2">Asc {orderBy === "asc" && <BiCheck className="inline float-right text-neutral-900" />}</li>
+            <li onClick={() => onChangeOrderBy("desc")} className="cursor-pointer px-4 text-neutral-500 hover:bg-neutral-400 hover:text-white py-2">Desc {orderBy === "desc" && <BiCheck className="inline float-right text-neutral-900" />}</li>
         </ul>
     );
 }
 
 const Search = () => {
-    const { query, onChangeQuery } = useContext(AptListContext)
+    const { query, onChangeQuery, sortBy, orderBy, onChangeSortBy, onChangeOrderBy } = useContext(AptListContext)
     const [toggleDropDown, setToggleDropDown] = useState(false)
 
     return (
@@ -32,7 +33,7 @@ const Search = () => {
                 className="border-2 border-neutral-200 rounded-l-md pl-8 appearance-none h-10 w-9/12 sm:w-10/12" />
             <button onClick={() => setToggleDropDown(!toggleDropDown)}
                 className="bg-blue-400 text-white py-2 px-2 w-3/12 sm:w-2/12"> Sort By <BiArrowToBottom className="inline" /> </button>
-            <DropDown toggle={toggleDropDown} />
+            <DropDown toggle={toggleDropDown} sortBy={sortBy} orderBy={orderBy} onChangeSortBy={(mySort) => onChangeSortBy(mySort)} onChangeOrderBy={(mySort) => onChangeOrderBy(mySort)} />
         </div>
     );
 }
--- src/contexts/AptListContext.js
+++ src/contexts/AptListContext.js
@@ -8,72 +8,81 @@ const AptListContextProvider = ({ children }) => {
             "id": "1",
             "aptTitle": "Onboarding Meeting",
             "aptLocation": "Ikeja City Mall",
-            "aptDate": "08-10-2022 08:04 PM",
+            "aptDate": "2022-08-11 15:48",
             "aptNotes": "Be early and formally dressed."
         },
         {
             "id": "2",
             "aptTitle": "Hangout with besties",
             "aptLocation": "Rhapsody",
-            "aptDate": "08-10-2022 08:04 PM",
+            "aptDate": "2022-08-11 15:48",
             "aptNotes": "Dress classy. Take cash."
         },
         {
             "id": "3",
             "aptTitle": "Family brunch",
             "aptLocation": "Grandfather's house",
-            "aptDate": "08-10-2022 08:04 PM",
+            "aptDate": "2022-08-11 15:48",
             "aptNotes": "Buy potato pie for Grandmother"
         },
         {
             "id": "4",
             "aptTitle": "School health registration",
             "aptLocation": "Deans office",
-            "aptDate": "08-10-2022 08:04 PM",
+            "aptDate": "2022-08-11 15:48",
             "aptNotes": "Take all registration documents."
         },
         {
             "id": "5",
             "aptTitle": "Gym day",
             "aptLocation": "Hotel gym",
-            "aptDate": "08-10-2022 08:04 PM",
+            "aptDate": "2022-08-11 15:48",
             "aptNotes": "Take two bottles of water with you."
         },
         {
             "id": "6",
             "aptTitle": "Nail appointment",
             "aptLocation": "Sissy's nail salon",
-            "aptDate": "08-10-2022 08:04 PM",
+            "aptDate": "2022-08-11 15:48",
             "aptNotes": "Take a good book along. Book an apt for your next fix."
         },
         {
             "id": "7",
             "aptTitle": "Youth group",
             "aptLocation": "Church",
-            "aptDate": "08-10-2022 08:04 PM",
+            "aptDate": "2022-08-11 15:48",
             "aptNotes": "Bring your evangelism response slips along."
         },
         {
             "id": "8",
             "aptTitle": "Project meeting",
             "aptLocation": "Zoom",
-            "aptDate": "08-10-2022 08:04 PM",
+            "aptDate": "2022-08-11 15:48",
             "aptNotes": "Get a pen and paper ready."
         },
         {
             "id": "9",
             "aptTitle": "Class event dinner",
             "aptLocation": "Lecture theatre one",
-            "aptDate": "08-10-2022 08:04 PM",
+            "aptDate": "2022-08-11 15:48",
             "aptNotes": "Book your makeup. Get your dress fixed."
         }
     ]
     const [query, setQuery] = useState('')
-    const [aptList, setAptList] = useState(AppointmentList)
+    const [aptList, setAptList] = useState(() => {
+        const localData = localStorage.getItem('appointments')
+        return localData ? JSON.parse(localData) : AppointmentList
+    })
     const [filteredList, setFilteredList] = useState(aptList)
     const [sortBy, setSortBy] = useState("aptTitle")
     const [orderBy, setOrderBy] = useState("asc")
 
+    const onChangeSortBy = (mySort) => {
+        setSortBy(mySort)
+    }
+    const onChangeOrderBy = (mySort) => {
+        setOrderBy(mySort)
+    }
 
     const deleteItem = (id) => {
         const filteredList = aptList.filter(item => id !== item.id)
@@ -86,6 +95,7 @@ const AptListContextProvider = ({ children }) => {
 
     useEffect(() => {
         setFilteredList(aptList)
+        localStorage.setItem('appointments', JSON.stringify(aptList))
     }, [aptList])
 
     const onChangeQuery = (myQuery) => {
@@ -107,10 +117,10 @@ const AptListContextProvider = ({ children }) => {
                 )
             })
         )
-    }, [query, aptList])
+    }, [query, aptList, sortBy, orderBy])
 
     return (
-        <AptListContext.Provider value={{ aptList, deleteItem, addItem, query, onChangeQuery, filteredList }}>
+        <AptListContext.Provider value={{ aptList, deleteItem, addItem, query, onChangeQuery, filteredList, sortBy, onChangeSortBy, orderBy, onChangeOrderBy }}>
             {children}
         </AptListContext.Provider>
     )
